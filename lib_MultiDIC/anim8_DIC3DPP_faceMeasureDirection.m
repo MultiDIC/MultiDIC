b5e480fd4e2e9dc6f74f2e631f78ad1ad428943d
Dana Solav <30727421+danasolav@users.noreply.github.com>
2022-01-13 16:24:57 +0200

    update readme

--- lib_MultiDIC/anim8_DIC3DPP_faceMeasureDirection.m
+++ lib_MultiDIC/anim8_DIC3DPP_faceMeasureDirection.m
@@ -310,8 +310,6 @@ end
 % License: <https://github.com/MultiDIC/MultiDIC/blob/master/LICENSE.txt>
 % 
 % Copyright (C) 2018  Dana Solav
-% 
-% Modified by Rana Odabas 2018
-% 
+% % 
 % If you use the toolbox/function for your research, please cite our paper:
 % <https://engrxiv.org/fv47e>
